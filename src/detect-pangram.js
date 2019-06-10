626ae5b1362b085cbe8b1d24b584fa9836650d4e
vpolets <vpolets@gmail.com>
2019-06-10 15:49:41 +0300

    add task and tests

---
+++ src/detect-pangram.js
@@ -0,0 +1,21 @@
+'use strict';
+
+/**
+ * Implement detectPangram function:
+ *
+ * A pangram is a sentence that contains every single letter of the alphabet
+ * at least once. For example, the sentence "The quick brown fox jumps over
+ * the lazy dog" is a pangram, because it uses the letters A-Z at least once
+ * (case is irrelevant).
+ * Given a string, detect whether or not it is a pangram. Return True if it
+ * is, False if not. Ignore numbers and punctuation.
+ *
+ * @param {string} phrase
+ *
+ * @return {boolean}
+ */
+function detectPangram(phrase) {
+  // write code here
+}
+
+module.exports = detectPangram;
